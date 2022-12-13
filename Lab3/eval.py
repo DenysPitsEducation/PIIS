7c29a5644c348e69a1a9727739fc27a87aebea14
d.pits <d.pits@smartweb.com.ua>
2022-12-14 00:32:04 +0200

    Change path

--- Lab3/eval.py
+++ Lab3/eval.py
@@ -4,7 +4,7 @@ import chess
 import chess.engine
 
 def getEval(board):
-    engine = chess.engine.SimpleEngine.popen_uci(r'/Users/d.pits/Documents/Education/PIIS/Lab3/stockfish-8-mac/Mac/stockfish-8-64')
+    engine = chess.engine.SimpleEngine.popen_uci(r'/Users/d.pits/PycharmProjects/PIIS/Lab3/stockfish-8-mac/Mac/stockfish-8-64')
     score = engine.analyse(board, chess.engine.Limit(time=0.01))['score'].black().score()
     engine.quit()
 
